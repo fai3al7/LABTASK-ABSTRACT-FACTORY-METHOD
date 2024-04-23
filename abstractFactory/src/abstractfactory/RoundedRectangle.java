0790db2b986f46abca40525964bd720983512271
fai3al7 <114581600+fai3al7@users.noreply.github.com>
2024-04-23 23:14:42 +0500

    Add files via upload

---
+++ abstractFactory/src/abstractfactory/RoundedRectangle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractfactory;
+
+/**
+ *
+ * @author SP21-BSE-028
+ */
+public class RoundedRectangle implements Shape{
+
+    @Override
+    public void draw() {
+       System.out.println("RoundedRectangle"); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
